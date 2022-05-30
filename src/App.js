17de765c06feaf645d25babe8910caf05580e29d
khanh-ngoc <ngochkFX15406@funix.edu.vn>
2022-05-30 22:45:41 +0900

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
+//function App () {
+//        return (
+//            <div className="App">
+//                <Navbar dark color="primary" >
+//                    <div className="container">
+//                        <NavbarBrand href="/">First ReactApp ever</NavbarBrand>
+//                    </div>
+//                </Navbar>
+//            </div>
+//        );
+//};
 
-
-function App () {
+class App extends Component {
+    render() {
         return (
             <div className="App">
-                <Navbar dark color="primary" >
+                <Navbar dark color="primary">
                     <div className="container">
-                        <NavbarBrand href="/">First ReactApp ever</NavbarBrand>
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
             </div>
         );
-};
+    }
+}
 
     export default App;
 
