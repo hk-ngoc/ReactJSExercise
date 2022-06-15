c46cabc6f7d6898801e4fa4abb57d8107aa49231
khanh-ngoc <ngochkFX15406@funix.edu.vn>
2022-06-15 11:04:06 +0900

    1st commit for ASM1

---
+++ src/components/StaffListComponents.js
@@ -0,0 +1,37 @@
+﻿import React, { Component } from react;
+import { STAFFS } from './shared/staffs';
+
+
+class StaffList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			staffdetail: STAFFS
+		};
+
+	}
+
+	render() {
+		const stafflist = this.state.staffdetail.map((stafflist) => {
+			return (
+				<div key={stafflist.id} className="col-12 col-md-6 col-lg-4">
+					<Card className="mt-1">
+						<CardTitle>{stafflist.name}</CardTitle>
+					</Card>
+				</div>
+			)
+		}
+	)
+		return (
+			<div className="container">
+				<div className="row">{stafflist}</div>
+				<div className="row">
+					<p>Bấm vào tên nhân viên để xem chi tiết</p>
+				</div>
+			</div>
+		
+			)
+	}
+}
+
+export default StaffList;
