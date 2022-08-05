45390216ed29a49f4d67361e374b4a19dc48d6c9
khanh-ngoc <ngochkFX15406@funix.edu.vn>
2022-08-05 10:46:20 +0900

    last commit for lab 4

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {}
-
-	}
-	render() {
+	renderDish(dish) {
 		return (
-			<div className="row" >
-	{/*for imgs and description*/}
-				<div className="col-12 col-md-5 m-1">
-					<Card>
-						<CardImg width="100%" src={this.props.dishdetail.image} alt={this.props.dishdetail.name} />
-						<CardBody>
-							<CardTitle>{this.props.dishdetail.name}</CardTitle>
-							<CardText>{this.props.dishdetail.description}</CardText>
-						</CardBody>
-					</Card>
+				<Card>
+					<CardImg width="100%" src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+			)		
+	};
+
+	renderComment(comment) {
+    	return (
+				
+				<ul className="list-unstyled">
+					{comment.map((commentdetail) => {
+						return (<div key={commentdetail.id}>
+							<li>
+								<p>{commentdetail.comment}</p>
+								<p>--{commentdetail.author},{commentdetail.date}</p>
+							</li>
+						</div>
+						)
+					}
+				)
+			}
+				</ul>
+		)			
+	}
+	
+
+	render() {
+		if (this.props.dish != null) {
+			return (
+				<div className="container">
+					<div className="row" >
+						{/*for imgs and description*/}
+						<div className="col-12 col-md-5 m-1">
+							{this.renderDish(this.props.dish)}
+						</div>
+
+						{/*for comments*/}
+						<div className="col-12 col-md-5 m-1">
+							<h4>Comments</h4>
+							{this.renderComment(this.props.dish.comments)}
+
+						</div>
+					</div >
 				</div>
-	{/*for comments*/}
-				<div className="col-12 col-md-5 m-1"></div>
-			</div >
-		)
+
+			)
+		} else {
+			return (<div></div>)
+        }
+		
 	}
 
 };
