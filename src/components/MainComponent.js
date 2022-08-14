8882961509a25ee64e4f1a32b648c487195212da
khanh-ngoc <ngochkFX15406@funix.edu.vn>
2022-08-15 00:15:25 +0900

    finished lab502 single-page-application part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -15,13 +19,20 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
+            comments: COMMENTS,
         };
     }
 
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+
+                />
             )
             
         }
@@ -31,6 +42,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/Menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/Contactus' component={Contact}/>
                     <Redirect to='/home'/>
                 </Switch>
                 <Footer/>
