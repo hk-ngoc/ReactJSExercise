514f030ef64ae8fae842e848c3861ca8cac2c581
khanh-ngoc <ngochkFX15406@funix.edu.vn>
2022-08-22 10:30:19 +0900

    Add sortSalary function

--- src/components/SalarySheetComponent.js
+++ src/components/SalarySheetComponent.js
@@ -1,24 +1,32 @@
 ﻿import React from 'react';
 import { Card, CardTitle, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
 
 
 function RenderSalarySheet({ staff }) {
-		return (
+	const salcount = (staff.salaryScale * 3000000 + staff.overTime * 200000).toFixed(0);
+
+	return (
 		<Card>
 			<CardBody>
 				<CardTitle>{staff.name}</CardTitle>
 				<CardText>Mã nhân viên: {staff.id}</CardText>
 				<CardText>Hệ số lương: {staff.salaryScale}</CardText>
 					<CardText>Số ngày làm thêm: {staff.overTime}</CardText>
-					<CardText className="shadow bg-info p-2">LƯƠNG: {(staff.salaryScale * 3000000 + staff.overTime * 200000).toFixed(0)}</CardText>
+					<CardText className="shadow bg-info p-2">LƯƠNG: {salcount}</CardText>
 			</CardBody>
 		</Card>
 	);
 }
 
 const SalarySheet = (props) => {
-	const salsheet = props.staff.map((staff) => {
+	const [sortSalary, setSortSalary] = useState(false);
+
+	const salsheet = props.staff.sort(
+		(a, b) => sortSalary ? a.salaryScale - b.salaryScale : b.salaryScale - a.salaryScale).map(
+			(staff) => {
 		return (
 			<div key={staff.id} className="col-12 col-sm-6 col-md-4 mt-4 mb-3">
 				<RenderSalarySheet staff={staff} />
@@ -39,9 +47,12 @@ const SalarySheet = (props) => {
 				</Breadcrumb>
 			</div>
 			<div className="row">
-				<div className="col-12">
+				<div className="col-12 col-sm-4">
 					<h3 className="mt-3">BẢNG LƯƠNG NHÂN VIÊN</h3>
 					<hr />
+				</div>
+				<div className="col-12 col-sm-4">
+					<button className="mt-3" onClick={() => setSortSalary(!sortSalary)}>Xếp theo HSL</button>
 				</div>
 			</div>
 			<div className="row">
